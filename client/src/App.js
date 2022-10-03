c73c3902ddeb15af371ab85fb0f74e11fcd1ea41
farjana01 <nipa.farzana87@gmail.com>
2022-10-04 00:25:52 +0600

    changed app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
 
 function App() {
+  const [form, setForm] = useState(
+                                    {
+                                      amount:0,
+                                      description:'',
+                                      date:''
+                                    }
+                                  )
+  const handleChange = (e) => {
+    setForm({...form, [e.target.name]:e.target.value});
+  }
+  const handleSubmit = async (e) =>{
+    e.preventDefault();
+    const res = await fetch('http://localhost:4000/transaction', {
+      method: 'POST',
+      body: form,
+    });
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input name="amount" value={form.amount} type="number" onChange={handleChange} placeholder="Enter transaction amount" />
+        <input name='description' value={form.description} type="text" onChange={handleChange} placeholder="Enter transaction details" />
+        <input name='date' type="date" value={form.date} onChange={handleChange} />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }

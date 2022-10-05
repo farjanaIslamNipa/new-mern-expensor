23377cd3d7de27a7f7c2cbdc797c80292578be02
farjana01 <nipa.farzana87@gmail.com>
2022-10-05 16:29:57 +0600

    transactions api created

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,27 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 
 function App() {
   const [form, setForm] = useState(
-                                    {
-                                      amount:0,
-                                      description:'',
-                                      date:''
-                                    }
-                                  )
+    {
+      amount:0,
+      description:'',
+      date:''
+    }
+  );
+
+  const [transactions, setTransactions] = useState([])
+
+  useEffect(() => {
+    fetchTransactions()
+  },[transactions]);
+
+  async function fetchTransactions(){
+    const res = await fetch('http://localhost:4000/transaction')
+    const data = await res.json();
+    setTransactions(data.data);
+    console.log(data.data)
+  }
+          
   const handleChange = (e) => {
     setForm({...form, [e.target.name]:e.target.value});
   }
@@ -15,8 +29,13 @@ function App() {
     e.preventDefault();
     const res = await fetch('http://localhost:4000/transaction', {
       method: 'POST',
-      body: form,
+      body: JSON.stringify(form),
+      headers:{
+        'content-type': 'application/json'
+      }
     });
+    const data = await res.json()
+    console.log(data, 'response')
   }
   return (
     <div>
@@ -26,6 +45,26 @@ function App() {
         <input name='date' type="date" value={form.date} onChange={handleChange} />
         <button type="submit">Submit</button>
       </form>
+
+      <hr />
+      <table>
+        <tbody>
+          <tr>
+            <th>Amount</th>
+            <th>Description</th>
+            <th>Date</th>
+          </tr>
+          {
+            transactions.map((trx) => (
+              <tr key={trx.id}>
+            <td>{trx.amount}</td>
+            <td>{trx.description}</td>
+            <td>{trx.date}</td>
+          </tr>
+            ))
+          }
+        </tbody>
+      </table>
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -2,16 +2,34 @@ import express from 'express';
 const PORT = 4000;
 import mongoose from 'mongoose';
 import cors from 'cors';
+import bodyParser from 'body-parser';
+import Transaction from './models/transaction.js';
 
 
 const app = express();
 app.use(cors());
+app.use(bodyParser.json());
 
 await mongoose.connect('mongodb+srv://farjana:Mahveernawshad1@mern-expensor.mjidtyo.mongodb.net/?retryWrites=true&w=majority');
 console.log('mongodb connected');
 
 app.get("/", (req, res) => {
   res.send("Hello world");
+});
+
+app.get('/transaction', async (req, res) => {
+  const transaction = await Transaction.find({}).sort({createdAt: -1})
+  res.json({data:transaction})
+})
+app.post("/transaction", async (req, res) => {
+  const {amount, description, date} = req.body;
+  const transaction = new Transaction({
+    amount,
+    description,
+    date,
+  });
+  await transaction.save();
+  res.json({message:"Success"});
 });
 
 app.listen(PORT, ()=>{
